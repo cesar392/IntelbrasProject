1719d4180103bddf8904413233729c144ef97b6b
Cesar Comelli <cesar.comelli@dynamox.net>
2023-05-10 21:05:57 -0300

    Add colors

---
+++ IntelbrasProject/IntelbrasProject/Utils/Colors.swift
@@ -0,0 +1,36 @@
+//
+//  Colors.swift
+//  IntelbrasProject
+//
+//  Created by Cesar Comelli on 10/05/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var backgroundColor: UIColor { return UIColor(hexString: "#263238") }
+    static var backgroundColorLight: UIColor { return UIColor(hexString: "#2F3D44") }
+    static var colorPrimary: UIColor { return UIColor(hexString: "#00A335") }
+    static var lightGray: UIColor { return UIColor(hexString: "#9DB3BD") }
+}
+
+extension UIColor {
+    convenience init(hexString: String) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt32()
+        Scanner(string: hex).scanHexInt32(&int)
+        let a, r, g, b: UInt32
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+}
