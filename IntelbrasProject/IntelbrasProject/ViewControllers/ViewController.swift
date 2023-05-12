eb23151388ebd7829f3b4f53d3e1dd24bafcc7de
Cesar Comelli <cesar.comelli@dynamox.net>
2023-05-12 09:59:33 -0300

    feat: update main storyboard view and ViewController

--- IntelbrasProject/IntelbrasProject/ViewControllers/ViewController.swift
+++ IntelbrasProject/IntelbrasProject/ViewControllers/ViewController.swift
@@ -9,14 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Variables
+    @IBOutlet weak var dashboardButton: UIButton!
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var videoButton: UIButton!
+    @IBOutlet weak var alarmButton: UIButton!
+
+    lazy var devicesList: [Device] = {
+        var deviceList = [Device]()
+        deviceList.append(contentsOf: AlarmCentral.getMockAlarmCentrals())
+        deviceList.append(contentsOf: VideoDevice.getMockVideoDevices())
+        return deviceList
+    }()
+
+    // MARK: - UIComponents
+
+
+    // MARK: - LifeCyle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.view.backgroundColor = .defaultBackground
     }
+
+    // MARK: - Setup Targets
+    @IBAction func didTapDashboard(_ sender: Any) { didTapDashboard() }
+    @IBAction func didTapFavorite(_ sender: Any) {  didTapFavorite() }
+    @IBAction func didTapVideo(_ sender: Any) { didTapVideo() }
+    @IBAction func didTapAlarm(_ sender: Any) { didTapAlarm() }
+
+    // MARK: - Setup Actions
+    private func didTapDashboard() {
+    }
+
+    private func didTapFavorite() {
+    }
+
+    private func didTapVideo() {
+    }
+
+    private func didTapAlarm() {
+    }
+
+
+    // MARK: - UI Setup
 
 }
